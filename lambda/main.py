02ac431c976b2720de5e18bbd569c9c9c3ffe2d8
Jimin <jimin.park@student.unsw.edu.au>
2026-03-20 01:20:29 +1100

    added router

--- lambda/main.py
+++ lambda/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
 from mangum import Mangum
-from routers import collect, preprocess, public
+from routers import collect, preprocess, public, analysis
 
 app = FastAPI()
 
 app.include_router(collect.router)
 app.include_router(preprocess.router)
 app.include_router(public.router)
+app.include_router(analysis.router)
 
 handler = Mangum(app)
